54727f3d8c7bdfc064fb66647da681d30ea4661d
soypat <graded.sp@gmail.com>
2021-01-11 09:14:40 -0300

    typo

--- state/arithmetic.go
+++ state/arithmetic.go
@@ -68,7 +68,7 @@ func Max(s State) float64 {
 	return floats.Max(s.x)
 }
 
-// Max returns the minimum value of s
+// Min returns the maximum value of s
 func Min(s State) float64 {
 	return floats.Min(s.x)
 }
